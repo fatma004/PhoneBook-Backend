fff9b71e3957f9a6dca844cdd4d7598ca358205a
Fatma04 <36867393+fatma004@users.noreply.github.com>
2022-06-18 20:18:49 +0200

    update api

---
+++ BL/Services/AuthService.cs
@@ -0,0 +1,173 @@
+﻿using AutoMapper;
+using BL.Bases;
+using BL.Configuration;
+using BL.Dto;
+using BL.Interfaces;
+using DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationDbContext _context;
+        private readonly JWT _jwt;
+
+        public AuthService(UserManager<AppUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IOptions<JWT> jwt,
+            IConfiguration configuration,
+            ApplicationDbContext context,
+            IMapper mapper) : base(mapper)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            _context = context;
+            _jwt = jwt.Value;
+        }
+
+
+        [Obsolete]
+        public async Task<AuthModel> RegisterAsync(RegisterModel model)
+        {
+            string Role = "User";
+            var userbyEmail = await _userManager.FindByEmailAsync(model.Email);
+            if (userbyEmail != null)
+                return new AuthModel { Message = " This Email is already used. " };
+            var userbyName = await _userManager.FindByNameAsync(model.UserName);
+
+            if (userbyName != null)
+                return new AuthModel { Message = "Enter another username Beacause it is used" };
+
+            if (model.Password != model.ConfirmPassword)
+                return new AuthModel { Message = "Password and ConfirmPassword Don't match!" };
+            var user = new AppUser
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                AccessFailedCount = 0,
+                PhoneNumberConfirmed = false,
+                
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                var errors = string.Empty;
+
+                foreach (var error in result.Errors)
+                    errors += $"{error.Description},";
+
+                return new AuthModel { Message = errors };
+            }
+            await _userManager.AddToRoleAsync(user, Role);
+
+            var jwtSecurityToken = await CreateJwtToken(user);
+
+            var confirmEmailToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            var encodedEmailToken = Encoding.UTF8.GetBytes(confirmEmailToken);
+            var validEmailToken = WebEncoders.Base64UrlEncode(encodedEmailToken);
+
+            
+            return new AuthModel
+            {
+                Email = user.Email,
+                ExpiresOn = jwtSecurityToken.ValidTo,
+                IsAuthenticated = true,
+                Roles = new List<string> { Role },
+                Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken),
+                UserName = user.UserName,
+                UserId = user.Id,
+
+            };
+        }
+
+        public async Task<AuthModel> LoginAsync(LoginModel model)
+        {
+            var authModel = new AuthModel();
+
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                authModel.Message = "  Email or Password is not correct";
+                return authModel;
+            }
+
+            var jwtSecurityToken = await CreateJwtToken(user);
+            var rolesList = await _userManager.GetRolesAsync(user);
+
+            authModel.IsAuthenticated = true;
+            authModel.Token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+            authModel.Email = user.Email;
+            authModel.UserName = user.UserName;
+            authModel.ExpiresOn = jwtSecurityToken.ValidTo;
+            authModel.Roles = rolesList.ToList();
+            authModel.UserId = user.Id;
+            return authModel;
+        }
+
+        public async Task<string> AddRoleAsync(AddRoleModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+
+            if (user is null || !await _roleManager.RoleExistsAsync(model.Role))
+                return "Invalid user ID or Role";
+
+            if (await _userManager.IsInRoleAsync(user, model.Role))
+                return "User already assigned to this role";
+
+            var result = await _userManager.AddToRoleAsync(user, model.Role);
+
+            return result.Succeeded ? string.Empty : "Something wrong";
+        }
+
+        private async Task<JwtSecurityToken> CreateJwtToken(AppUser user)
+        {
+            var userClaims = await _userManager.GetClaimsAsync(user);
+            var roles = await _userManager.GetRolesAsync(user);
+            var roleClaims = new List<Claim>();
+
+            foreach (var role in roles)
+                roleClaims.Add(new Claim("roles", role));
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id)
+            }
+            .Union(userClaims)
+            .Union(roleClaims);
+
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwt.Key));
+            var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: _jwt.Issuer,
+                audience: _jwt.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(_jwt.DurationInDays),
+                signingCredentials: signingCredentials);
+
+            return jwtSecurityToken;
+        }
+    }
+}
---
+++ DAL/Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Models
+{
+   public class AppUser : IdentityUser
+    {
+        public virtual ICollection<Contact>? Contacts { get; set; }
+    }
+}
